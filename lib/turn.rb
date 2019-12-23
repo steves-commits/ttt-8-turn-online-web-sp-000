a6396739675065dae8415885dfcf058c8936b1b9
Steven Topazio <sat@northpointprinting.com>
2019-12-23 15:51:38 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,9 @@
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]}"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{borad[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} " 
   
 
 
