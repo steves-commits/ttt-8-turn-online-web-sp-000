8a8d557a70031b71f68d45466e3b61101cc5a92b
Steven Topazio <sat@northpointprinting.com>
2019-12-23 16:25:44 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,8 +35,22 @@ else
 end
 end
 
+def move(board, index, character = "X")
+  board[index] = character
+  return board
+end
 
 
 def turn(board)
   puts "Please enter 1-9"
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index) == TRUE
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
 end
